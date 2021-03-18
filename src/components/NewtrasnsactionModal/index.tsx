0102cf7417d422ba1e8d9ab6308e8d0871533358
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-03-18 20:22:24 -0300

    Estrutura do formulário

--- src/components/NewtrasnsactionModal/index.tsx
+++ src/components/NewtrasnsactionModal/index.tsx
@@ -13,8 +13,23 @@ export function NewtrasnsactionModal({isOpen, onRequestClose}:NewtrasnsactionMod
         <Modal
             isOpen={isOpen}
             onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
         >
+
+            <Container>
+
             <h2>Cadastra transação</h2>
+            <input placeholder="Título"/>
+            
+            <input type="number" placeholder="Valor" />
+
+            <input placeholder="Categoria"/>
+
+           <button type="submit">Cadastrar</button>
+
+            </Container>
+
         </Modal>
     )
 }
